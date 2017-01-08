92474b763568901f0e463e30f9c01d9ddb924a97
trinaleigh <trinaleigh@users.noreply.github.com>
2017-01-07 23:56:22 -0500

    add countdowm

--- clock.js
+++ clock.js
@@ -1,3 +1,12 @@
 function startTimer(){
-	console.log("clicked!")
+	setInterval(countDown,1000);
+}
+
+var secondsLeft = 25*60;
+var timerDisplay = document.getElementById("countdown");
+timerDisplay.innerHTML = secondsLeft;
+
+function countDown(){
+	secondsLeft -= 1;
+	timerDisplay.innerHTML = secondsLeft;
 }
