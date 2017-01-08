c12dfabe5050627b9c792dc7ea77478343f74215
trinaleigh <trinaleigh@users.noreply.github.com>
2017-01-08 00:39:38 -0500

    play sound

--- clock.js
+++ clock.js
@@ -1,5 +1,13 @@
+var gong = new Audio('audio/gong.mp3');
+
+var startButton = document.getElementById("button")
+
 function startTimer(){
 	setInterval(countDown,1000);
+	gong.play()
+	startButton.disabled = true;
+	startButton.style.backgroundColor = "#BA004C";
+
 }
 
 var secondsLeft = 25*60;
